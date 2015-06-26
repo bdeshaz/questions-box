044006bd83c7a7aa0be7b7d19c654dfc58d52d6b
Alan R <alan.rominger@gmail.com>
2015-06-26 12:19:57 -0400

    trying to figure out login

--- qbox/users/urls.py
+++ qbox/users/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
+import django.contrib.auth as auth_views
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)', DetailView.as_view(model=User), name='view_user'),
@@ -11,6 +12,7 @@ urlpatterns = [
                             form_class=UserCreationForm,
                             success_url = "http://www.google.com"),
                             name='register'),
+    url(r'^login/', auth_views.login, name='login'),
 
     # url(r'^', include('django.contrib.auth.urls')),
     # url(r'^login/$', 'django.contrib.auth.views.login', name='view_login'),
