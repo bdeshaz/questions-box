9380d75b0a450c062ac073e0512b13e201e7a882
Brian <bdeshaz@gmail.com>
2015-06-28 14:31:05 -0400

    added ability to update user email and password

--- qbox/users/views.py
+++ qbox/users/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from qa.models import Answer
+from .forms import UserForm
 
 
 class UserDetailView(generic.DetailView):
@@ -24,3 +27,25 @@ class UserDetailView(generic.DetailView):
         context["user"] = self.the_user
 
         return context
+
+
+@login_required
+def edit_user(request):
+    user = request.user
+    if request.method == "GET":
+        user_form = UserForm(instance=user)
+    elif request.method == "POST":
+        user_form = UserForm(instance=user, data=request.POST)
+        if user_form.is_valid():
+            n_user = user_form.save()
+            password = user.password
+            n_user.set_password(password)
+            n_user.save()
+            n_user = authenticate(username=n_user.username,
+                                  password=password)
+            login(request, n_user)
+            messages.add_message(request, messages.SUCCESS,
+                                 "Your profile has been updated!")
+            return redirect('/questions/')
+
+    return render(request, "users/edit_user.html", {"form": user_form})
